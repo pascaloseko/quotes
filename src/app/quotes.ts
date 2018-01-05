c7a7af886d6936f4f47efb621a4e013d37015199
cospamental <cospa18@gmail.com>
2018-01-05 13:19:26 +0300

    my quotes edit 1

---
+++ src/app/quotes.ts
@@ -0,0 +1,7 @@
+export class Quotes {
+    public showRatings:boolean;
+    constructor(public id: number, public title: string,public rating: string){
+        this.showRatings=false
+    }
+}
+
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-import { Quotes } from './quotes';
+import { Component, OnInit } from '@angular/core';
+import { Quotes } from '../quotes'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-quotes',
+  templateUrl: './quotes.component.html',
+  styleUrls: ['./quotes.component.css']
 })
-export class AppComponent {
+export class QuotesComponent implements OnInit {
   quotes = [
     new Quotes(1,"What you do makes a difference, and you have to decide what kind of difference you want to make","+1"),
     new Quotes(2,"Think Big And Don’t Listen To People Who Tell You It Can’t Be Done. Life’s Too Short To Think Small","+2"),
@@ -14,4 +14,14 @@ export class AppComponent {
     new Quotes(4,"You Can Develop Any Habit Or Thought Or Behavior That You Consider Desirable Or Necessary","+1"),
     new Quotes(4,"We Become What We Think About","+2"),
   ]
+
+  upvote(index){
+    this.quotes[index].showRatings = !this.quotes[index].showRatings;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
 }
