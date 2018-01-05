2fb425931a516a8de698f57a93ea61da04ff9770
cospamental <cospa18@gmail.com>
2018-01-05 17:11:19 +0300

    add pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
     QuoteFormComponent,
+    DateCountPipe,
   ],
   imports: [
     BrowserModule
