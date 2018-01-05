e20465f20160a893bfcf900294db5f9aa814934a
cospamental <cospa18@gmail.com>
2018-01-06 01:00:58 +0300

    finished phase 6

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -16,7 +16,7 @@ export class QuotesComponent implements OnInit {
     new Quotes(2,"Think Big And Don’t Listen To People Who Tell You It Can’t Be Done. Life’s Too Short To Think Small","+2",new Date(2018,6,9)),
     new Quotes(3,"It Doesn’t Matter Where You Came From. All That Matters Is Where You Are Going","+3",new Date(2018,1,12)),
     new Quotes(4,"You Can Develop Any Habit Or Thought Or Behavior That You Consider Desirable Or Necessary","+1",new Date(2018,0,18)),
-    new Quotes(4,"We Become What We Think About","+2",new Date(2018,2,14)),
+    new Quotes(5,"We Become What We Think About","+2",new Date(2018,2,14)),
   ]
 
 upvote(index){
