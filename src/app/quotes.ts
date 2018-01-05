a87f55c7b842527688e0566709f1aec434b594c1
cospamental <cospa18@gmail.com>
2018-01-05 18:30:34 +0300

    finished phase 3

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,6 +1,6 @@
 export class Quotes {
     public showRatings:boolean;
-    constructor(public id: number, public title: string,public rating: string,public completeDate:Date){
+    constructor(public id: number, public name: string,public rating: string,public completeDate:Date){
         this.showRatings=false
     }
 }
